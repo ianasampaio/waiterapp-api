0d213eccddc8d86e0decadc6e861fae510ca4c23
ianasampaio <iana.sampaio06@gmail.com>
2024-12-04 10:38:19 -0300

    feat: add webSockets implementation

--- src/app/useCases/orders/listOrders.ts
+++ src/app/useCases/orders/listOrders.ts
@@ -3,7 +3,18 @@ import { Order } from "../../models/Order";
 
 export async function listOrders(req: Request, res: Response) {
   try {
-    const orders = await Order.find()
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
+
+    const tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
+    const orders = await Order.find({
+      createdAt: {
+        $gte: today,
+        $lt: tomorrow
+      }
+    })
     .sort({ createdAt: 1})
     .populate('products.product');
 
